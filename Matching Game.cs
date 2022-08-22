703d318c37f8b7940555d568d3eae96d91f13a5d
Nilderson Chavez <nildersonalexis@gmail.com>
2022-08-21 20:08:47 -0600

    segunda parte de codigo para asignar los iconos

--- Matching Game.cs
+++ Matching Game.cs
@@ -10,24 +10,44 @@ using System.Windows.Forms;
 
 namespace semana4_POO_y_controles_de_formularios
 {
-    public partial class Form1 : Form
 
+
+    public partial class Form1 : Form
     {
+
+
+        Label firstClicked = null;
+
+        Label secondClicked = null;
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+    {
+        "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+    };
         public Form1()
         {
             InitializeComponent();
 
-            InitializeComponent();
+            AssignIconsToSquares();
         }
-
-        private void label10_Click(object sender, EventArgs e)
+        private void AssignIconsToSquares()
         {
 
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
