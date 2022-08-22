91e2f42b69ab5954cc6cc665efaa2a6d05702908
Nilderson Chavez <nildersonalexis@gmail.com>
2022-08-21 20:13:58 -0600

    aqui agregue las funciones a los Label

--- Matching Game.cs
+++ Matching Game.cs
@@ -14,40 +14,590 @@ namespace semana4_POO_y_controles_de_formularios
 
     public partial class Form1 : Form
     {
+        
+        
+            Label firstClicked = null;
 
+            Label secondClicked = null;
 
-        Label firstClicked = null;
+            Random random = new Random();
 
-        Label secondClicked = null;
-
-        Random random = new Random();
-
-        List<string> icons = new List<string>()
+            List<string> icons = new List<string>()
     {
         "!", "!", "N", "N", ",", ",", "k", "k",
         "b", "b", "v", "v", "w", "w", "z", "z"
     };
-        public Form1()
-        {
-            InitializeComponent();
-
-            AssignIconsToSquares();
-        }
-        private void AssignIconsToSquares()
-        {
-
-            foreach (Control control in tableLayoutPanel1.Controls)
+            public Form1()
             {
-                Label iconLabel = control as Label;
-                if (iconLabel != null)
+                InitializeComponent();
+
+                AssignIconsToSquares();
+            }
+            private void AssignIconsToSquares()
+            {
+
+                foreach (Control control in tableLayoutPanel1.Controls)
                 {
-                    int randomNumber = random.Next(icons.Count);
-                    iconLabel.Text = icons[randomNumber];
-                    iconLabel.ForeColor = iconLabel.BackColor;
-                    icons.RemoveAt(randomNumber);
+                    Label iconLabel = control as Label;
+                    if (iconLabel != null)
+                    {
+                        int randomNumber = random.Next(icons.Count);
+                        iconLabel.Text = icons[randomNumber];
+                        iconLabel.ForeColor = iconLabel.BackColor;
+                        icons.RemoveAt(randomNumber);
+                    }
                 }
             }
-        }
 
+            private void label6_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label2_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label3_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label4_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label5_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label7_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label8_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label9_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label10_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label11_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label12_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label13_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label14_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label15_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+
+            private void label16_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+
+                    if (firstClicked == null)
+                    {
+                        firstClicked = clickedLabel;
+                        firstClicked.ForeColor = Color.Black;
+
+                        return;
+                    }
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+
+                    CheckForWinner();
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        return;
+                    }
+
+                }
+            }
+
+            }
+
+
+        }
     }
-}
+    
+ 
+
+    
+
+
+
+
